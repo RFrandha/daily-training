160c895bfbedf4867ebe1cb0313128ac65f7bd5e
RFrandha <restow.frandha@gmail.com>
2025-02-13 01:36:28 +0700

    Improve leetcode problem 2

--- leetcode/lc_2.go
+++ leetcode/lc_2.go
@@ -13,6 +13,37 @@ func L2() {
 	log.Println(addTwoNumbers(l1, l2))
 }
 
+// func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+// 	result := &ListNode{0, nil}
+// 	currentL1 := l1
+// 	currentL2 := l2
+// 	tempVal := 0
+// 	ph := result
+
+// 	for currentL1 != nil || currentL2 != nil || tempVal > 0 {
+
+// 		if currentL1 != nil {
+// 			tempVal += currentL1.Val
+// 		}
+// 		if currentL2 != nil {
+// 			tempVal += currentL2.Val
+// 		}
+
+// 		ph.Next = &ListNode{tempVal % 10, nil}
+// 		ph = ph.Next
+// 		tempVal /= 10
+
+// 		if currentL1 != nil {
+// 			currentL1 = currentL1.Next
+// 		}
+// 		if currentL2 != nil {
+// 			currentL2 = currentL2.Next
+// 		}
+// 	}
+
+// 	return result.Next
+// }
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	result := &ListNode{0, nil}
 	currentL1 := l1
@@ -24,21 +55,16 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 		if currentL1 != nil {
 			tempVal += currentL1.Val
+			currentL1 = currentL1.Next
 		}
 		if currentL2 != nil {
 			tempVal += currentL2.Val
+			currentL2 = currentL2.Next
 		}
 
 		ph.Next = &ListNode{tempVal % 10, nil}
 		ph = ph.Next
 		tempVal /= 10
-
-		if currentL1 != nil {
-			currentL1 = currentL1.Next
-		}
-		if currentL2 != nil {
-			currentL2 = currentL2.Next
-		}
 	}
 
 	return result.Next
